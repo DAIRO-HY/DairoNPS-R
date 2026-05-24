4abdbe21edf38da18f2eea5558b6729362d5466d
zhoulq <615741631@qq.com>
2026-05-25 00:05:18 +0900

    添加编译脚本

---
+++ build_docker_npc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#github登录票据
+github_token=$GITHUB_TOKEN
+
+#docker用户名
+docker_user=$DOCKER_USER
+docker_pwd=$DOCKER_PASSWORD
+
+#项目名
+projectName="DairoNPC"
+
+
+# 获取版本号
+#version=$(grep -oP '(?<=version = ")[^"]+' ./lib_npc/Cargo.toml)
+version=$(sed -n 's/^version = "\(.*\)"/\1/p' ./lib_npc/Cargo.toml)
+
+
+#---------------------------------------上传Docker镜像-----------------------------------------
+echo "正在打包Docker镜像..."
+cp ./document/docker-npc-build/Dockerfile .
+
+docker login -u $docker_user --password $docker_pwd
+
+#前提条件
+#创建新的 builder
+ #docker buildx create --name mybuilder --driver docker-container --use
+ #docker buildx inspect --bootstrap
+docker buildx build --platform linux/amd64,linux/arm64 -t $docker_user/dairo-npc:$version --push .
+#docker push $docker_user/dairo-npc:$version
+docker logout
+
+echo "---------------------------------------docker镜像推送完成--------------------------------------"
