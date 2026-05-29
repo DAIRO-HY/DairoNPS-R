e0bc0ac8bdad1ca9c3b1186c4243cb9fa9dcbdb2
zhoulq <615741631@qq.com>
2026-05-29 23:41:01 +0900

    添加编译脚本

--- document/build/build_npc.sh
+++ document/build/build_npc.sh
@@ -10,7 +10,6 @@ project_root="$(cd "$script_dir/../.." && pwd)"
 develop_tool_dir=$(echo ~)/develop_tool
 
 # 获取版本号
-#version=$(grep -oP '(?<=version = ")[^"]+' ./lib_npc/Cargo.toml)
 version=$(sed -n 's/^version = "\(.*\)"/\1/p' $project_root/lib_npc/Cargo.toml)
 
 echo "script_dir:$script_dir"
@@ -22,23 +21,23 @@ echo "version:$version"
 #先创建build文件夹
 mkdir $develop_tool_dir/build
 
-#apt update
-#yes|apt install build-essential
-#
-##--------------------------------------安装CARGO--------------------------------------START# 指定安装目录
-#export RUSTUP_HOME=$develop_tool_dir/.rustup
-#export CARGO_HOME=$develop_tool_dir/.cargo
-#
-## 加入PATH
-#export PATH=${CARGO_HOME}/bin:${PATH}
-#curl https://sh.rustup.rs -sSf | sh -s -- -y     --default-toolchain 1.93.0
-#
-#cargo --version
-##--------------------------------------安装CARGO--------------------------------------START
+apt update
+yes|apt install build-essential
 
-##打包amd64位linux二进制文件
-#cargo --version && cargo clean && cargo build --release --package DairoNPC
-#mv $project_root/target/release/DairoNPC $project_root/build/DairoNPC-linux-amd64-$version
+#--------------------------------------安装CARGO--------------------------------------START# 指定安装目录
+export RUSTUP_HOME=$develop_tool_dir/.rustup
+export CARGO_HOME=$develop_tool_dir/.cargo
+
+# 加入PATH
+export PATH=${CARGO_HOME}/bin:${PATH}
+curl https://sh.rustup.rs -sSf | sh -s -- -y     --default-toolchain 1.93.0
+
+cargo --version
+#--------------------------------------安装CARGO--------------------------------------START
+
+#打包amd64位linux二进制文件
+cargo --version && cargo clean && cargo build --release --package DairoNPC
+mv $project_root/target/release/DairoNPC $project_root/build/DairoNPC-linux-amd64-$version
 
 
 ##打包amd32位linux二进制文件
