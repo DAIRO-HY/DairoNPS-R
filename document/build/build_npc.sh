8f0994c694026f8e960c6847d496bd8cf9c0b562
zhoulq <615741631@qq.com>
2026-05-29 23:37:52 +0900

    添加编译脚本

--- document/build/build_npc.sh
+++ document/build/build_npc.sh
@@ -15,26 +15,26 @@ version=$(sed -n 's/^version = "\(.*\)"/\1/p' $project_root/lib_npc/Cargo.toml)
 
 echo "script_dir:$script_dir"
 echo "project_root:$project_root"
-echo "pdevelop_tool_dir:$pdevelop_tool_dir"
+echo "develop_tool_dir:$develop_tool_dir"
 echo "version:$version"
 
 
 #先创建build文件夹
 mkdir $develop_tool_dir/build
 
-apt update
-yes|apt install build-essential
-
-#--------------------------------------安装CARGO--------------------------------------START# 指定安装目录
-export RUSTUP_HOME=$develop_tool_dir/.rustup
-export CARGO_HOME=$develop_tool_dir/.cargo
-
-# 加入PATH
-export PATH=${CARGO_HOME}/bin:${PATH}
-curl https://sh.rustup.rs -sSf | sh -s -- -y     --default-toolchain 1.93.0
-
-cargo --version
-#--------------------------------------安装CARGO--------------------------------------START
+#apt update
+#yes|apt install build-essential
+#
+##--------------------------------------安装CARGO--------------------------------------START# 指定安装目录
+#export RUSTUP_HOME=$develop_tool_dir/.rustup
+#export CARGO_HOME=$develop_tool_dir/.cargo
+#
+## 加入PATH
+#export PATH=${CARGO_HOME}/bin:${PATH}
+#curl https://sh.rustup.rs -sSf | sh -s -- -y     --default-toolchain 1.93.0
+#
+#cargo --version
+##--------------------------------------安装CARGO--------------------------------------START
 
 ##打包amd64位linux二进制文件
 #cargo --version && cargo clean && cargo build --release --package DairoNPC
