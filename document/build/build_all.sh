7c59bfe462201087ad5b591ce54a1e8c365223f8
zhoulq <615741631@qq.com>
2026-05-30 23:10:16 +0900

    添加编译脚本

--- document/build/build_all.sh
+++ document/build/build_all.sh
@@ -3,9 +3,9 @@
 # 进入项目根目录
 cd "$(dirname "$0")/../.."
 
-#./document/build/build_npc.sh
+./document/build/build_npc.sh
 ./document/build/build_npc_android.sh
-#./document/build/build_nps.sh
+./document/build/build_nps.sh
 ./document/build/build_github.sh
-#./document/build/build_npc_docker.sh
-#./document/build/build_nps_docker.sh
+./document/build/build_npc_docker.sh
+./document/build/build_nps_docker.sh
