60b4f5b8bc807beda16b440aef09c723d952f5ef
zhoulq <615741631@qq.com>
2026-06-06 00:46:31 +0900

    添加编译脚本

--- document/build/build_npc.sh
+++ document/build/build_npc.sh
@@ -19,7 +19,7 @@ echo $MASTER_SRC_DIR
 
 
 #打包amd64位linux二进制文件
-docker run --platform linux/amd64 --rm -v $MASTER_DEVELOP_DIR/project/DairoNPS-R:/home/rust/src -v $MASTER_DEVELOP_DIR/.cargo:~/.cargo -w /home/rust/src rust:1.93 \
+docker run --platform linux/amd64 --rm -v $MASTER_DEVELOP_DIR/project/DairoNPS-R:/home/rust/src -v $MASTER_DEVELOP_DIR/.cargo:/root/.cargo -w /home/rust/src rust:1.93 \
 sh -c "cargo clean && cargo build --release --package DairoNPS"
 #mv ./target/release/DairoNPS ./build/DairoNPS-linux-amd64-$version
 
