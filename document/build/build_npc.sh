de7c8535d6e00d271d8176118806fc2550650527
zhoulq <615741631@qq.com>
2026-06-06 01:14:30 +0900

    添加编译脚本

--- document/build/build_npc.sh
+++ document/build/build_npc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#前提条件 安装 QEMU binfmt,目的:在amd64位linux系统上编译arm64位linux二进制文件
+#docker run --privileged --rm tonistiigi/binfmt --install all
+
+
 # 当前 sh 文件所在目录
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
@@ -22,6 +26,12 @@ docker_rust_build(){
   sh -c "cargo clean && cargo build --release --package DairoNPS"
 }
 
+
+#docker run --platform linux/arm64 --rm -v /home/docker/jenkins+jdk17+docker/data/develop_tool/project/DairoNPS-R:/home/rust/src -v /home/docker/jenkins+jdk17+docker/data/develop_tool/.cargo:/root/.cargo -w /home/rust/src rust:1.93 \
+#  sh -c "cargo clean && cargo build --release --package DairoNPS"
+#
+#docker run --platform linux/amd64 --rm -v /home/docker/jenkins+jdk17+docker/data/develop_tool/project/DairoNPS-R:/home/rust/src -v /home/docker/jenkins+jdk17+docker/data/develop_tool/.cargo:/root/.cargo -w /home/rust/src rust:1.93 \
+#  sh -c "cargo clean && cargo build --release --package DairoNPS"
 
 #打包amd64位linux二进制文件
 #docker_rust_build linux/amd64
